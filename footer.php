24f1a779f82312e1bbeb60959091590fef70a729
izzyasi <isabellypenteado23@gmail.com>
2025-11-22 11:03:10 -0300

    refactor: atualiza o footer

--- footer.php
+++ footer.php
@@ -1,20 +1,13 @@
 <?php
-/*
- * Documentação: Rodapé (footer.php)
- */
 ?>
 <footer class="main-footer">
     <div class="footer-container">
         
         <nav class="footer-nav">
-            <a href="#">Contact Us</a>
-            <a href="#">Customer Service</a>
-            <a href="#">Store Locator</a>
-            <a href="#">Legal Notice</a>
         </nav>
         
         <div class="footer-copyright">
-            <p>&copy; 2025 Roseglaze</p>
+            <p>&copy; <?php echo date('Y'); ?> Roseglaze. Todos os direitos reservados.</p>
         </div>
 
     </div>
